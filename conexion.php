717f27748cc853f8e0c182d6426d33d214aee790
Pame1238 <32856495+Pame1238@users.noreply.github.com>
2018-11-29 23:29:56 -0300

    Create conexion.php

---
+++ conexion.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="estiloDos.css">
+</head>
+<body>
+	<div id="out">
+		<input class="log-btn" type="submit" onclick="location.href='logout.php';" value="Cerrar Sesión"/>
+	</div>
+	<div>
+		<img src="store.png">
+	</div>
+	
+	<h1 >Sistema Kiosco Vivi</h1>
+
+	<nav>
+		<ul id="menu">
+			<li><a href="empleados.php">Empleados</a>
+			</li>
+			<li><a href="productos.php">Productos</a>
+			</li>
+			<li><a href="proveedores.php">Proveedores</a>
+			</li>
+			<li><a href="clientes.php">Clientes</a>	
+			</li>
+			<li><a href="ventas.php">Ventas</a>
+			</li>
+		</ul>
+	</nav>
+	
+</body>
+</html>
